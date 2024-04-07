6e15e9832ce6e1abe3ba5bf16b4807d4e11ee90d
RuslanaBelyakova <deep-crusher@mail.ru>
2024-04-07 18:07:44 +0300

    first commit

--- WebApplication3/WeatherForecast.cs
+++ WebApplication3/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace WebApplication3
 
         public int TemperatureC { get; set; }
         public int asdad;
-        усеосрюкн
+      
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
